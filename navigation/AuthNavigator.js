fffcf9429f7e388aac7b22ef3471f2ac639fcaa1
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2019-09-24 16:55:14 -0400

    refactor(del): deleting unused file

--- navigation/AuthNavigator.js
+++
@@ -1,8 +0,0 @@
-import {createStackNavigator} from "react-navigation";
-import LoginScreen from "../screens/LoginScreen";
-
-export const AuthStack = createStackNavigator(
-    {
-        Login: LoginScreen,
-    }
-);
