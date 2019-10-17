7ef17ee2b6f3cb8287a8493ce9c91b61f0b311bd
FRANCK Pierre-Antoine <pierre-antoine.franck@epitech.eu>
2019-10-17 14:27:13 -0400

    feat(tabbar): editing tab bar navigator

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,4 +1,4 @@
-const tintColor = '#2f95dc';
+const tintColor = '#8FDD3D';
 
 export default {
   tintColor,
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,76 +1,101 @@
 import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
-
+import {Platform, Image} from 'react-native';
+import {createStackNavigator, createBottomTabNavigator} from 'react-navigation';
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import ProductsScreen from '../screens/ProductsScreen';
+import GrowersScreen from '../screens/GrowersScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const config = Platform.select({
-  web: { headerMode: 'screen' },
-  default: {},
+    web: {headerMode: 'screen'},
+    default: {},
 });
 
-const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  config
+const ProductStack = createStackNavigator(
+    {
+        Product: ProductsScreen,
+    },
+    config
 );
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
-      }
-    />
-  ),
+ProductStack.navigationOptions = {
+    tabBarLabel: 'Produits',
+    tabBarOptions: {
+        activeTintColor: '#5CC04A',
+        inactiveTintColor: '#ccc',
+    },
+    tabBarIcon: ({focused}) => {
+        const image = focused
+            ? require('../assets/images/TabBarNavigation/Products/active.png')
+            : require('../assets/images/TabBarNavigation/Products/inactive.png');
+        return (
+            <Image
+                source={image}
+            />
+        )
+    }
 };
 
-HomeStack.path = '';
+ProductStack.path = '';
 
-const LinksStack = createStackNavigator(
-  {
-    Links: LinksScreen,
-  },
-  config
+const GrowerStack = createStackNavigator(
+    {
+        Grower: GrowersScreen,
+    },
+    config
 );
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
+GrowerStack.navigationOptions = {
+    tabBarLabel: 'Producteurs',
+    tabBarOptions: {
+        activeTintColor: '#5CC04A',
+        inactiveTintColor: '#ccc',
+    },
+    tabBarIcon: ({focused}) => {
+        const image = focused
+            ? require('../assets/images/TabBarNavigation/Producteurs/active.png')
+            : require('../assets/images/TabBarNavigation/Producteurs/inactive.png');
+        return (
+            <Image
+                source={image}
+            />
+        )
+    }
 };
 
-LinksStack.path = '';
+GrowerStack.path = '';
 
-const SettingsStack = createStackNavigator(
-  {
-    Settings: SettingsScreen,
-  },
-  config
+const ProfileStack = createStackNavigator(
+    {
+        Profile: ProfileScreen,
+    },
+    config
 );
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
+ProfileStack.navigationOptions = {
+    tabBarLabel: 'Profil',
+    tabBarOptions: {
+        activeTintColor: '#5CC04A',
+        inactiveTintColor: '#ccc',
+    },
+    tabBarIcon: ({focused}) => {
+        const image = focused
+            ? require('../assets/images/TabBarNavigation/Profil/active.png')
+            : require('../assets/images/TabBarNavigation/Profil/inactive.png');
+        return (
+            <Image
+                source={image}
+            />
+        )
+    }
 };
 
-SettingsStack.path = '';
+ProfileStack.path = '';
 
 const tabNavigator = createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+    ProductStack,
+    GrowerStack,
+    ProfileStack,
 });
 
 tabNavigator.path = '';
