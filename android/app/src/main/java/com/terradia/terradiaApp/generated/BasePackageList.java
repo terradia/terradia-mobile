dbe8df8007f5467be544de5bc9ba725c1a2c2e19
Damien <damsltc57@yahoo.fr>
2020-12-04 00:07:20 +0100

    fix(android): fixed android build

--- android/app/src/main/java/com/terradia/terradiaApp/generated/BasePackageList.java
+++ android/app/src/main/java/com/terradia/terradiaApp/generated/BasePackageList.java
@@ -7,18 +7,27 @@ import org.unimodules.core.interfaces.Package;
 public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
+        new expo.modules.application.ApplicationPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.facebook.FacebookPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.gl.GLPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localization.LocalizationPackage(),
         new expo.modules.location.LocationPackage(),
+        new expo.modules.notifications.NotificationsPackage(),
+        new expo.modules.payments.stripe.StripePackage(),
         new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.sharing.SharingPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
-        new expo.modules.updates.UpdatesPackage()
+        new expo.modules.updates.UpdatesPackage(),
+        new expo.modules.webbrowser.WebBrowserPackage()
     );
   }
 }
